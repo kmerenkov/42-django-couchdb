b7b06ef2e9bbedda71c20355ca70ec2fd8d6bd33
Sergey Kishchenko <voidwrk@gmail.com>
2009-03-21 18:10:50 +0200

    ticket:1: Test for model saving was added

---
+++ django_couchdb/djcouchtest/core/tests/test_queries.py
@@ -0,0 +1,13 @@
+from django.core.management import call_command
+from nose.tools import assert_equal
+
+from djcouchtest.core.models import Boo
+
+class TestQueries:
+    def test_saveobject(self):
+        call_command('syncdb', interactive=False, verbosity=0)
+        b = Boo()
+        b.title = "First Title"
+        b.slug = "first_title"
+        b.save()
+        assert True
