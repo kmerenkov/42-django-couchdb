001cb8713173166704d6a7f52210c4a320bd9a7b
Sergey Kishchenko <voidwrk@gmail.com>
2009-03-22 15:46:47 +0200

    ticket:1: -Some tests were added

--- django_couchdb/djcouchtest/core/tests/test_queries.py
+++ django_couchdb/djcouchtest/core/tests/test_queries.py
@@ -1,12 +1,13 @@
 from django.core.management import call_command
 from nose.tools import assert_equal
 
-from djcouchtest.core.models import Boo
+from djcouchtest.core.models import Boo, Foo
 
 class TestQueries:
     def test_saveobject(self):
         call_command('syncdb', interactive=False, verbosity=0)
         Boo.objects.all().delete()
+        Foo.objects.all().delete()
         assert_equal(Boo.objects.all().count(), 0)
         b = Boo()
         b.title = "First Title"
@@ -18,3 +19,13 @@ class TestQueries:
         b2.save()
         assert_equal(Boo.objects.all().count(), 2)
         assert_equal(Boo.objects.filter(slug="first_title").count(),1)
+        f = Foo(boo=b)
+        f.save()
+        assert_equal(b.foo_set.count(), 1)
+        assert_equal(b2.foo_set.count(), 0)
+        f2 = Foo(boo=b2)
+        f2.save()
+        new_f = Foo.objects.filter(boo=b)[0]
+        assert new_f.boo.title == "First Title"
+
+
